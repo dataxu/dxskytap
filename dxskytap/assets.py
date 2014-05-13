14df7dc92c44b0b8a5a8515cb3f48634ba91881c
Ryan Carey <rcarey@dataxu.com>
2014-05-13 16:26:24 -0400

    fix #14, unused import for dxskytap.restobject.RestObject

--- dxskytap/assets.py
+++ dxskytap/assets.py
@@ -23,7 +23,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from dxskytap.restobject import RestMap, RestObject, RestAttribute
+from dxskytap.restobject import RestMap, RestAttribute
 from dxskytap.assignableobject import AssignableObject
 
 class Asset(AssignableObject):

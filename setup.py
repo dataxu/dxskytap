c8451b8f9fa9d975a00121c029b22d9340b7530e
Ryan Carey <rcarey@dataxu.com>
2014-12-05 16:37:11 -0500

    update setup.py for pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 """
-(c) Copyright 2013. DataXu, Inc. All Rights Reserved.
+(c) Copyright 2013, 2014. DataXu, Inc. All Rights Reserved.
+:license:   BSD-new, see LICENSE for more details.
 
 dx_skytap module setup and package.
 """
@@ -11,6 +12,7 @@ setup(
     author='DataXu',
     author_email='rcarey@dataxu.com',
     description='DataXu Utilities for Skytap',
+    long_description=long_description,
     license='BSD-new',
     packages=['dxskytap'],
     package_data={'dxskytap': ['*.pem']},
@@ -18,5 +20,19 @@ setup(
     tests_require=['nose','coverage'],
     test_suite='test.all',
     url='http://www.dataxu.com',
-    version='0.9.2-SNAPSHOT'
+    version='1.0.0-SNAPSHOT',
+    keywords='skytap cloud client rest api development',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ],
 )

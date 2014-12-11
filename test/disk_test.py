a32dfe653821a4ca81720831f05ca0e9823f6645
Ryan Carey <rcarey@dataxu.com>
2014-12-11 11:55:03 -0500

    fix test cleanup

--- test/disk_test.py
+++ test/disk_test.py
@@ -17,44 +17,40 @@ class TestDisk(unittest.TestCase):
         self.configurations = self.root.configurations()
         template = self.root.templates()['294385']
         self.assertTrue(template is not None, "Unable to get template 294385")
+        self.config1 = template.create_configuration()
+        self.config1.wait_for()
+        self.config2 = template.create_configuration()
+        self.config2.wait_for()
 
     def test_createNewDisk(self):
-        template = self.root.templates()['294385']
-        self.assertTrue(template is not None, "Unable to get template 294385")
-        config1 = template.create_configuration()
-        config1.wait_for()
-        vm1 = config1.vms().values()[0]
+        vm1 = self.config1.vms().values()[0]
         self.assertEqual(len(vm1.hardware().disks()), 1,
             "Expected only 1 initial disk in vm")
         vm1.hardware().addDisk(8192)
-        config1.wait_for()
+        self.config1.wait_for()
         vm1.refresh()
         self.assertEqual(len(vm1.hardware().disks()), 2,
             "Expected 2 disks in vm")
         config1.delete()
 
     def test_DeleteDisk(self):
-        template = self.root.templates()['294385']
-        self.assertTrue(template is not None, "Unable to get template 294385")
-        config1 = template.create_configuration()
-        config1.wait_for()
-        vm1 = config1.vms().values()[0]
+        vm1 = self.config2.vms().values()[0]
         self.assertEqual(len(vm1.hardware().disks()), 1,
             "Expected only 1 initial disk in vm")
         vm1.hardware().addDisk(8192)
-        config1.wait_for()
+        self.config2.wait_for()
         vm1.refresh()
         self.assertEqual(len(vm1.hardware().disks()), 2,
             "Expected 2 disks in vm")
         vm1.hardware().disks()[1].delete()
-        config1.wait_for()
+        self.config2.wait_for()
         vm1.refresh()
         self.assertEqual(len(vm1.hardware().disks()), 1,
             "Expected 1 disks in vm")
-        config1.delete()
 
     def tearDown(self):
-        pass
+        self.config1.delete()
+        self.config2.delete()
 
 def suite():
     suite = unittest.TestSuite()

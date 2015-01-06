8e247dd13fde339bebcf2944cf9bbdd05f601fd1
Eugene Muzykin <emuzykin@dataxu.com>
2015-01-06 16:20:30 -0500

    pr feedback - fixing formatting

--- dxskytap/company.py
+++ dxskytap/company.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013, DataXu Inc.
+# Copyright (c) 2014, DataXu Inc.
 # All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
@@ -23,26 +23,23 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from dxskytap.restobject import RestMap, RestObject, RestAttribute
+from dxskytap.restobject import RestObject
 
 class Company(RestObject):
     def __init__(self, connect):
-        super(Company, self).__init__(connect,
-            "company/quotas")
+        super(Company, self).__init__(connect, "company/quotas")
 
     def company_quota_request(self):
-        return Company_Quota(self._connect, self._resource, self.alldata(),
-            self)
+        return Company_Quota(self._connect, self._resource, self.alldata(), self)
 
 
 class Company_Quota(RestObject):
     def __init__(self, connect, res, initial_data, parent):
-        super(Company_Quota, self).__init__(connect, res, initial_data,
-            is_full=True) # parent=parent, parent_attr="hardware"
+        super(Company_Quota, self).__init__(connect, res, initial_data, is_full=True)
 
-        self.list_of_resources_with_limits = ('concurrent_svms', 'concurrent_vms', 'concurrent_storage_size', 'cumulative_svms', 
-            'concurrent_public_ips', 'concurrent_networks')
-        self.quota_fields = ('quota_type','units','limit','usage','subscription','max_limit')
+        self.list_of_resources_with_limits = ('concurrent_svms', 'concurrent_vms', 'concurrent_storage_size',
+        'cumulative_svms', 'concurrent_public_ips', 'concurrent_networks')
+        self.quota_fields = ('quota_type', 'units', 'limit', 'usage', 'subscription', 'max_limit')
         self.company_quota_data_structure = {}
 
         for group in initial_data:
